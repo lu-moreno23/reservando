b21fa18a45b71819dbe9cf4b871971b6c9168a9c
Lucia Moreno <marialuciams89@gmail.com>
2019-05-11 17:49:46 -0300

    Testeo funcion reservar horario
    Tests unitarios de:
    - Reservo un horario > se elimina correctamente del array
    - Reservar un horario inexistente > el lenght del array se mantiene igual
    - Reservar parametro vacio > el length se mantiene igual

--- js/tests.js
+++ js/tests.js
@@ -1,21 +1,37 @@
-var expect = chai.expect;
+let expect = chai.expect;
 
 //Tests reservarHorario(horario)
 //Estos tests tienen en comun que se prueba el array
 
 describe('Tests del array contenido en reservarHorario(horario)', function(){
     it('Al reservar un horario, el horario es eliminado del arreglo', function(){
-        let horariosSinReservaciones = Restaurant.horarios;
-        listado.reservarUnHorario(13);
-        //Restaurant.horarios();
-        expect(Restaurant.horarios).to.equal(horariosSinReservaciones-1);
+        
+        const testPruebaReservaciones = listado.restaurantes[0];
+        //reservo un horario con antelacion, es decir que el lenght pasaria de 3 a 2
+        testPruebaReservaciones.reservarHorario('13:00');
+
+        expect(testPruebaReservaciones.horarios[0]).to.equal('15:30');
+        expect(testPruebaReservaciones.horarios[1]).to.equal('18:00');
+        expect(testPruebaReservaciones.horarios.length).to.equal(2);
 
     });
-    /*it('Al intentar reservar un horario que no el restaurant no posee, el arreglo se mantiene igual', function(){
+    it('Al intentar reservar un horario que no el restaurant no posee, el arreglo se mantiene igual', function(){
+        const testReservarHorarioInexistente = listado.restaurantes[1];
+        //intento reservar un horario inexistente
+        testReservarHorarioInexistente.reservarHorario('16:00');
+
+        //el lenght del array se mantiene igual
+        expect(testReservarHorarioInexistente.horarios.length).to.equal(3);
 
     });
     it('Al intentar reservar un horario sin pasarle un parametro a la funcion, el arreglo se mantiene igual', function(){
+        const testPasoParametroVacio = listado.restaurantes[2];
+        //intento reservar un horario vacio
+        testPasoParametroVacio.reservarHorario('');
 
-    });*/
-})
+        //el lenght del array se mantiene igual
+        expect(testPasoParametroVacio.horarios.length).to.equal(3);
+
+    });
+});
 
